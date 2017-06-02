fc1c9298e1379ad5b635a09b4c8a60dc403f4eef
TangChangcheng <tangcc1127@gmail.com>
2017-06-02 20:03:18 +0800

    Q value

--- SquareEnvironment-0-master/Qvalue.py
+++ SquareEnvironment-0-master/Qvalue.py
@@ -1,5 +1,7 @@
 
 import numpy as np
+import pandas as pd
+import copy
 
 from Environment import Env
 
@@ -10,9 +12,11 @@ actions = np.arange(-5, 5, interval)
 
 # Q = {i: {} for i in status}
 
-Q = np.zeros([len(status), len(actions)]) + np.nan
+Q = np.zeros([len(status), len(actions)])
 
 gamma = 0.99
+coefs = [9.54151441, 6.8100, 2.76072571]
+
 
 def transf(state, action):
     s = int((state - min(status)) / interval)
@@ -37,16 +41,11 @@ def Qval(state, action, step):
         env.reset(status=state)
 
     qval += gamma * max(tmp)
-
     Q[s][a] = qval
-
-    print(qval)
-
     return qval
 
 
-with open('realQ', 'w') as fw:
-    import pandas as pd
+def write(path):
     for state in status:
         env.reset(status=state)
         for action in actions:
@@ -54,4 +53,35 @@ with open('realQ', 'w') as fw:
 
     a = pd.DataFrame(Q)
     a.to_csv('realQ.csv', encoding='utf8')
+
+def foo(X):
+    return coefs[0] * np.power(X, 2) + coefs[1] * X + coefs[2]
+
+
+GAMMA = 0.99
+R = copy.copy(Q)
+
+for i, s in enumerate(status):
+    for j, a in enumerate(actions):
+        R[i, j] = foo(s) - foo(s + a)
+
+
+def getMaxQ(state):
+    return max(Q[state, :])
+
+def QLearning(state):
+    for action in range(len(actions)):
+        s, a = transf(state, action)
+        Q[s,a] = R[s, a] + GAMMA * getMaxQ(s + a)
+
+
+for count in range(1000):
+    for i in range(len(status)):
+        QLearning(i)
+
+Q = pd.DataFrame(Q)
+Q.to_csv('realQ.csv')
+
+
+
 
