c509aef4c6244c92d4c29b0eec87873e1028b3da
TangChangcheng <tangcc1127@gmail.com>
2017-06-03 17:34:41 +0800

    dqn Q value

--- SquareEnvironment-0-master/Qval_plot.py
+++ SquareEnvironment-0-master/Qval_plot.py
@@ -3,19 +3,27 @@ import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import  matplotlib
+import json
 
 matplotlib.style.use('ggplot')
 
-interval = 0.01
-status = np.arange(-3, 3, interval)
-actions = np.arange(-5, 5, interval)
+from Qvalue import status, actions
 
 X, Y = np.meshgrid(status, actions)
 
 
-a = pd.read_csv('realQ.csv', index_col=0)
+with open('dqn_qvalue.json', 'r') as fr:
+    Z = np.array(json.load(fr)['q_value']).transpose()
+
+# a = pd.read_csv('realQ.csv', index_col=0)
+# Z = a.transpose()
+
 fig = plt.figure(1)
 ax = Axes3D(fig)
-ax.plot_surface(Y, X, a.transpose())
+ax.plot_surface(Y, X, Z)
+
+# fig = plt.figure(2)
+# ax = fig.add_subplot(111)
+# ax.plot(a.iloc[:10, :].transpose())
 
 plt.show()
--- SquareEnvironment-0-master/Qvalue.py
+++ SquareEnvironment-0-master/Qvalue.py
@@ -3,85 +3,59 @@ import numpy as np
 import pandas as pd
 import copy
 
-from Environment import Env
 
-env = Env()
-interval = 0.01
+interval = 0.05
 status = np.arange(-3, 3, interval)
-actions = np.arange(-5, 5, interval)
-
-# Q = {i: {} for i in status}
+actions = np.arange(-6, 6, interval)
 
 Q = np.zeros([len(status), len(actions)])
 
-gamma = 0.99
 coefs = [9.54151441, 6.8100, 2.76072571]
 
 
 def transf(state, action):
-    s = int((state - min(status)) / interval)
-    a = int((action - min(actions)) / interval)
+    s = int(np.around((state - min(status)) / interval))
+    a = int(np.around((action - min(actions)) / interval))
     return s, a
-
-def Qval(state, action, step):
-    _, reward, done, _ = env.step(np.array([action]))
-    qval = reward
-
-    s, a = transf(state, action)
-    if not np.isnan(Q[s, a]):
-        return Q[s, a]
-
-    if done or step >= 20:
-        Q[s][a] = qval
-        return qval
-
-    tmp = []
-    for ac in actions:
-        tmp.append(Qval(env.status[0], ac, step + 1))
-        env.reset(status=state)
-
-    qval += gamma * max(tmp)
-    Q[s][a] = qval
-    return qval
-
-
-def write(path):
-    for state in status:
-        env.reset(status=state)
-        for action in actions:
-            Qval(state, action, 0)
-
-    a = pd.DataFrame(Q)
-    a.to_csv('realQ.csv', encoding='utf8')
 
 def foo(X):
     return coefs[0] * np.power(X, 2) + coefs[1] * X + coefs[2]
 
 
-GAMMA = 0.99
+GAMMA = 0.5
 R = copy.copy(Q)
-
 for i, s in enumerate(status):
     for j, a in enumerate(actions):
         R[i, j] = foo(s) - foo(s + a)
 
 
 def getMaxQ(state):
+    if state < 0  or state >= len(status):
+        raise Exception('state is out of index')
     return max(Q[state, :])
 
 def QLearning(state):
-    for action in range(len(actions)):
+    for action in actions:
         s, a = transf(state, action)
-        Q[s,a] = R[s, a] + GAMMA * getMaxQ(s + a)
+        try:
+            Q[s,a] = R[s, a] + GAMMA * getMaxQ(int(np.around((state + action - min(status)) / interval)))
+        except Exception:
+            # print(state, action, state + action, s, a, s + a)
+            pass
 
+if __name__ == '__main__':
+    cur = 100
+    prev = 0
 
-for count in range(1000):
-    for i in range(len(status)):
-        QLearning(i)
+    #np.random.shuffle(status)
+    for count in range(5000):
+        for i in status:
+            QLearning(i)
+        prev = cur
+        cur =  (np.max(Q, axis=1).mean())
+        print(cur)
+        if abs(prev - cur) < 0.01 and count > 10:
+            break
 
-Q = pd.DataFrame(Q)
-Q.to_csv('realQ.csv')
-
-
-
-
+    Q = pd.DataFrame(Q)
+    Q.to_csv('realQ.csv')
